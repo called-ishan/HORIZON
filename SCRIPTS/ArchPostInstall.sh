dbc8d685b90b6ad1a826a0cab4fb43e6bf405855
Ishan <108247879+called-Ishan@users.noreply.github.com>
2024-07-20 19:20:49 +0530

    more

--- SCRIPTS/ArchPostInstall.sh
+++ SCRIPTS/ArchPostInstall.sh
@@ -21,12 +21,12 @@ sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
 sudo sed -i '/ParallelDownloads/s/^#/ILoveCandy\n/g' /etc/pacman.conf
 sudo sed -i '/VerbosePkgLists/s/^#//g' /etc/pacman.conf
 
-# Configuring bash
-cp /home/$USER/Downloads/configs/.bashrc /home/$USER
-
 #Configuring ohmybash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
 cp /home/$USER/Downloads/configs/90210.theme.sh /home/$USER/.oh-my-bash/themes/90210/
+
+# Configuring bash
+cp /home/$USER/Downloads/configs/.bashrc /home/$USER
 
 # Configuring nano
 cp /home/$USER/Downloads/configs/.nanorc /home/$USER
@@ -36,14 +36,18 @@ cp /home/$USER/Downloads/configs/user.js /home/$USER/Downloads
 
 # Removing temp files
 rm -rf /home/$USER/Downloads/configs
+rm -rf /home/$USER/Downloads/yay
 
 
 # Installing softwares
 yes | yay
-sudo pacman -S --noconfirm kvantum
-sudo pacman -S --noconfirm archlinux-tweak-tool-git
-sudo pacman -S --noconfirm gnome-disk-utilit
-sudo pacman -S --noconfirm firefox
-sudo pacman -S --noconfirm kdeconnect
-sudo pacman -S --noconfirm thorium-browser-bin
-sudo pacman -S --noconfirm youtube-music-bin
+yay -S --noconfirm archlinux-tweak-tool-git
+yay -S --noconfirm gnome-disk-utility
+yay -S --noconfirm firefox
+yay -S --noconfirm thorium-browser-bin
+yay -S --noconfirm youtube-music-bin
+
+# DE post-installation
+echo "Want to run DE post installation(y/n)"
+read ans
+echo "Choose your desktop"
