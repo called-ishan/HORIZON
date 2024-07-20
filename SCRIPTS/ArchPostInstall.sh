cd5f4a0cbc4c91d3b6c51bbbe57a84640ca711ea
Ishan <108247879+called-Ishan@users.noreply.github.com>
2024-07-20 18:14:39 +0530

    Update ArchPostInstall.sh

--- SCRIPTS/ArchPostInstall.sh
+++ SCRIPTS/ArchPostInstall.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
 # Installing yay
 sudo pacman -S --noconfirm git
 git clone https://aur.archlinux.org/yay.git && cd yay
 yes | makepkg -si
-clear
-echo "yay is Installed"
 
 #Copying my configs for later use
 cd /home/$USER/Downloads/
@@ -17,27 +14,19 @@ curl -o 90210.theme.sh https://raw.githubusercontent.com/called-Ishan/HORIZON/ma
 curl -o .nanorc https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/nanorc
 curl -o user.js https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/firefox.user.js
 
-
-# part 2 script after installing ohmybash
-echo "#!/bin/bash
-# editing ohmybash theme
-cp /home/$USER/Downloads/configs/90210.theme.sh /home/$USER/.oh-my-bash/themes/90210
-
-# Removing temp files
-rm -rf /home/$USER/Downloads/configs
-" >> /home/$USER/Downloads/postinstall-part2.sh
-
 sudo chmod +x /home/$USER/Downloads/postinstall-part2.sh
 
 #Editing pacman.conf
 sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
 sudo sed -i '/ParallelDownloads/s/^#/ILoveCandy\n/g' /etc/pacman.conf
 sudo sed -i '/VerbosePkgLists/s/^#//g' /etc/pacman.conf
-clear
-echo "pacman configured"
 
 # Configuring bash
 cp /home/$USER/Downloads/configs/.bashrc /home/$USER
+
+#Configuring ohmybash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
+cp /home/$USER/Downloads/configs/90210.theme.sh /home/$USER/.oh-my-bash/themes/90210/
 
 # Configuring nano
 cp /home/$USER/Downloads/configs/.nanorc /home/$USER
@@ -45,6 +34,16 @@ cp /home/$USER/Downloads/configs/.nanorc /home/$USER
 # Saving user.js for firefox
 cp /home/$USER/Downloads/configs/user.js /home/$USER/Downloads
 
-#Configuring ohmybash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" && ./home/$USER/Downloads/postinstall-part2.sh
+# Removing temp files
+rm -rf /home/$USER/Downloads/configs
 
+
+# Installing softwares
+yes | yay
+sudo pacman -S --noconfirm kvantum
+sudo pacman -S --noconfirm archlinux-tweak-tool-git
+sudo pacman -S --noconfirm gnome-disk-utilit
+sudo pacman -S --noconfirm firefox
+sudo pacman -S --noconfirm kdeconnect
+sudo pacman -S --noconfirm thorium-browser-bin
+sudo pacman -S --noconfirm youtube-music-bin
