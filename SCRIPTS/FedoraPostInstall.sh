2577bf9e3c91b87e23d26b6cc9b6210418dcd683
Ishan <108247879+called-ishan@users.noreply.github.com>
2024-09-04 18:08:52 +0530

    Update FedoraPostInstall.sh

--- SCRIPTS/FedoraPostInstall.sh
+++ SCRIPTS/FedoraPostInstall.sh
@@ -2,3 +2,12 @@ sudo dnf install libva-intel-driver
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
 sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf install vlc
+
+#Copying configs for later use
+cd /home/$USER/Downloads/
+mkdir configs
+cd configs
+curl -o .bashrc https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/bashrc
+curl -o 90210.theme.sh https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/90210.theme.sh
+curl -o .nanorc https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/nanorc
+curl -o user.js https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/firefox.user.js
