8cb4d951bb10d775c9f5b1e6dc874ef1d1acbd2d
Ishan <108247879+called-ishan@users.noreply.github.com>
2024-09-04 18:10:08 +0530

    Update FedoraPostInstall.sh

--- SCRIPTS/FedoraPostInstall.sh
+++ SCRIPTS/FedoraPostInstall.sh
@@ -11,3 +11,13 @@ curl -o .bashrc https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONF
 curl -o 90210.theme.sh https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/90210.theme.sh
 curl -o .nanorc https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/nanorc
 curl -o user.js https://raw.githubusercontent.com/called-Ishan/HORIZON/main/CONFIGS/firefox.user.js
+
+#Configuring ohmybash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
+cp /home/$USER/Downloads/configs/90210.theme.sh /home/$USER/.oh-my-bash/themes/90210/
+
+# Configuring bash
+cp /home/$USER/Downloads/configs/.bashrc /home/$USER
+
+# Configuring nano
+cp /home/$USER/Downloads/configs/.nanorc /home/$USER
