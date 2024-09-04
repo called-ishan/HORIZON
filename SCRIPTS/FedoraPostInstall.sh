37b558b8e597306fc6c617205c50b25068ee1648
Ishan <108247879+called-ishan@users.noreply.github.com>
2024-09-04 18:05:38 +0530

    Update SCRIPTS

---
+++ SCRIPTS/FedoraPostInstall.sh
@@ -0,0 +1,4 @@
+sudo dnf install libva-intel-driver
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf install vlc
